4102ba1a45133a237e5e0b71312dcaf9e5c75cd5
Dwiana Octavia <octaviadwiana@gmail.com>
2020-07-05 07:37:46 +0700

    edit web route

--- routes/web.php
+++ routes/web.php
@@ -10,32 +10,16 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-
-Route::get('/', function () {
-    return view('larahub.pertanyaan.form');
-});
-
-//----------
-
-Route::get('/data-tables', function () {
-    return view('tugasBlade.dt');
-});
-
-Route::get('/master', function () {
-    return view('adminlte.master');
-});
-
-Route::get('/items', function () {
-    return view('items.index');
-});
-
-//----------
+Route::get('/', 'PertanyaanController@index');
 
 Route::get('/pertanyaan', 'PertanyaanController@index');            //daftar pertanyaan
 Route::get('/pertanyaan/create', 'PertanyaanController@create');    //form pertanyaan
 Route::post('/pertanyaan', 'PertanyaanController@store');           //buat pertanyaan
-//Route::get('/pertanyaan/{id}', 'PertanyaanController@detail');
+Route::get('/pertanyaan/{id}', 'JawabanController@index');          //daftar jawaban di 1 pertanyaan
 Route::get('/jawaban/{id}', 'JawabanController@index');             //daftar jawaban di 1 pertanyaan (nnti diganti ke /pertanyaan{id})
 Route::get('/jawaban/create/{id}', 'JawabanController@create');     //form jawaban (id pertanyaan) [nanti diganti dengan /jawaban/{id}]
 Route::post('/jawaban/{id}', 'JawabanController@store');            //buat jawaban (id pertanyaan)
+
+Route::get('/pertanyaan/{id}/edit', 'PertanyaanController@edit');   //tampil form update pertanyaan
+Route::put('/pertanyaan/{id}', 'PertanyaanController@update');      //submit form pertanyaan dan kembali ke index
+Route::delete('/pertanyaan/{id}', 'PertanyaanController@delete');   //hapus pertanyaan
